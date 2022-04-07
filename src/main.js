0b56b0153a658c809741453da981609abdf112e7
goo-gy <lkukh17@naver.com>
2022-04-07 21:47:39 +0900

    function : chatting
    - 채팅 기능 제작
    - 입장시 알림
    - 입장시 참여자 목록 전달
    - 입장시 room 내의 다른 참여자에게도 알림

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const http = require('http');
 const SocketIO = require('socket.io');
-const cors = require('cors');
+const dayjs = require('dayjs');
 
 const PORT = 3001;
+const timeFormat = 'YYYY-MM-DD hh:mm';
+
+const event = {
+  connection: 'connection',
+  disconnect: 'disconnect',
+  joinRoom: 'join_room',
+  msg: 'msg',
+};
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -14,15 +22,38 @@ const wsServer = SocketIO(httpServer, {
   },
 });
 
-wsServer.on('connection', (socket) => {
-  console.log('socketID:', socket.id);
-  socket.on('room_join', ({ name }) => {
-    socket.join(name);
-    console.log(socket.rooms);
+const rooms = {};
+
+wsServer.on(event.connection, (socket) => {
+  socket.on(event.joinRoom, ({ userName, roomName }, done) => {
+    const time = dayjs().format(timeFormat);
+    socket.join(roomName);
+    if (rooms[roomName]) {
+      rooms[roomName] = [...rooms[roomName], userName];
+    } else {
+      rooms[roomName] = [userName];
+    }
+    // const peopleCount = wsServer.sockets.adapter.rooms.get(roomName)?.size;
+    // TODO : check same host
+    socket.to(roomName).emit(event.joinRoom, { userName, time });
+    done({ people: rooms[roomName], userName, time });
   });
 
-  socket.on('msg', ({ msg }) => {
-    socket.to('global').emit('msg', msg);
+  socket.on(event.msg, ({ userName, roomName, msg }, done) => {
+    const time = dayjs().format(timeFormat);
+    if (msg) {
+      socket.to(roomName).emit(event.msg, {
+        userName,
+        msg,
+        time,
+      });
+      done(time);
+    }
+  });
+
+  socket.on(event.disconnect, () => {
+    const peopleCount = wsServer.sockets.adapter.rooms.get('global')?.size;
+    if (peopleCount) socket.to('global').emit(event.joinRoom, { peopleCount });
   });
 });
 
